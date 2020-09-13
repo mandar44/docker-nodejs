c6833919f61e784bb222edbe822b809d6495dee2
Saurabh Rai <rsaurabh79@gmail.com>
2020-09-13 17:07:43 +0530

    Second Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('How are you doing');
+  res.send('DevOps Batch Docker lecture');
 });
 
 app.listen(8080, () => {
