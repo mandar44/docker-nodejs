6f3ba5fc028308e3d021b68bcf6c524a9407dc2b
mandar44 <65187434+mandar44@users.noreply.github.com>
2020-09-16 16:14:24 +0530

    Update 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Batch Docker lecture - Update 5');
+  res.send('DevOps Batch Docker lecture - Update 6');
 });
 
 app.listen(80, () => {
