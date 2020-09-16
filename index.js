b044e6c2967b09de1ad181b2bd5928c2558c1deb
mandar44 <65187434+mandar44@users.noreply.github.com>
2020-09-16 14:25:08 +0530

    V2 commit Update - 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Batch Docker lecture - Update 1');
+  res.send('DevOps Batch Docker lecture - Update 2');
 });
 
 app.listen(80, () => {
